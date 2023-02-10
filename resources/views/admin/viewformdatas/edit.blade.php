e1c4a361c8d79f85c497991b5e38098cfa4cb2d1
susan1800 <timalsinasusan14@gmail.com>
2023-02-10 07:36:10 +0545

    update

--- resources/views/admin/viewformdatas/edit.blade.php
+++ resources/views/admin/viewformdatas/edit.blade.php
@@ -215,6 +215,16 @@
 
 @endsection
 
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+      crossorigin="anonymous">
+</script>
+
     <script>
 
 
