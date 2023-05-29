ac7bcca9b72d5f93d77fbab39796592fb92e33ac
susan1800 <timalsinasusan14.com>
2023-05-29 21:20:58 +0545

    update

--- app/Imports/UsersPaymentImport.php
+++ app/Imports/UsersPaymentImport.php
@@ -18,12 +18,18 @@ class UsersPaymentImport implements ToModel, WithHeadingRow
     */
     public function model(array $row)
     {
-        $paymentstatus =new User();
-        $paymentstatus['name']=$row['name'];
-        $paymentstatus['registration_number']=$row['registration_number'];
-        $paymentstatus['roll_no'] = $row['roll_no'];
-        $paymentstatus['approve_form'] = '0';
-        $paymentstatus->save();
-        return $paymentstatus;
+        $user =new User();
+        $user['name']=$row['name'];
+        $user['registration_number']=$row['registration_number'];
+        $emailname = explode(" ",$row['name']);
+        $user['email'] = $row['roll_no'].strtolower($emailname[0])."@cosmoscollege.edu.np";
+        $user['roll_no'] = $row['roll_no'];
+        $user['password'] = $row['roll_no'];
+        $user['approve_form'] = '0';
+        $auth = Auth::where('title','user')->first();
+        $user['auth_id'] = $auth->id;
+        $user->save();
+        // dd($user);
+        return $user;
     }
 }
